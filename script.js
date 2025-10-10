db1934bcebb8261a9ea6e9120beec21c0c88d3aa
Ben <bkennedy23999@gmail.com>
2025-10-10 16:03:44 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -11,6 +11,20 @@ function escapeHtml(str) {
   return str.replaceAll("&","&amp;").replaceAll("<","&lt;").replaceAll(">","&gt;");
 }
 
+// smooth scroll for anchor links
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+        window.scrollTo({
+      top: target.offsetTop - 50, // offset for header
+      behavior: 'smooth'
+    });
+  });
+}
+
 // =========================================
 // PROJECTS - Fetch GitHub Repos
 // =========================================
