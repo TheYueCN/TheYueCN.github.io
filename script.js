56a6167251e585980465eaa1378c4784b5743535
Ben <bkennedy23999@gmail.com>
2025-10-10 16:15:24 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -10,7 +10,20 @@ const PROJECTS_COUNT = 6;
 function escapeHtml(str) {
   return str.replaceAll("&","&amp;").replaceAll("<","&lt;").replaceAll(">","&gt;");
 }
-
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener("click", function (e) {
+          e.preventDefault();
+          const target = document.querySelector(this.getAttribute("href"));
+          if (target) {
+            window.scrollTo({
+               top: target.offsetTop - 50,
+               behavior: "smooth"
+            });
+          }
+        });
+    });
+});
 // =========================================
 // PROJECTS - Fetch GitHub Repos
 // =========================================
